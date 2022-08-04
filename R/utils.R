640ba8b62d165cbe6a359c1002508a8056967189
Taylor Spaulding <TSpaulding@esassoc.com>
2022-08-04 09:50:16 -0700

    utility function descritpions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,14 @@
+#' Compare two Detection Dataframes
+#'
+#' This function compares two detection dataframes from filtered acoustic
+#' telemetry detections and identifies the filtered out detections. The first
+#' input is the original dataframe, the second is the filtered dataframe. Then
+#' it creates a dataframe of detections which are missing from the filtered
+#' dataframe.
+#'
+#' @param a1 unfiltered detection dataframe
+#' @param a2 filtered detection dataframe
+#' @return A dataframe of detections from the original dataframe missing from the filtered dataframe. Spurious detections.
 #' @export
 compare_df  <- function(a1,a2){
   a3 <- dplyr::select(a1, Tag_Hex, DateTime_Local)
@@ -8,6 +19,13 @@ compare_df  <- function(a1,a2){
   return(a1.without.a2.rows)
 }
 
+#' Calculate Standard Deviation Over a Rolling Window of 3
+#'
+#' This function is called within the third filter, and used to calculate a
+#' rolling standard deviation with a window including the 2 leading values.
+#'
+#' @param x an indexed position of a value in a vector for which the rolling standard deviation is needed
+#' @return the standard deviation of x, and the two leading values
 #' @export
 rolling_sd_3 <- function(x){
   mean = (x + lead(x) + lead(x,2))/3
